efd46bcc249b35c11bc1ef432bd51d71c43655ff
John Malcolm Anderson <jmanderson@verizonmedia.com>
2021-01-25 09:25:16 +0000

    CSS Modules

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import "./App.css";
+import classes from "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -43,6 +43,7 @@ class App extends Component {
 
   render() {
     let persons = null;
+    let btnClass = [classes.Button];
 
     if (this.state.showPersons ){
       persons = (
@@ -57,22 +58,23 @@ class App extends Component {
           })}
         </div>
       )
+      btnClass.push(classes.Red);
       // style.backgroundColor = 'red';
     }
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2){
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1){
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
     
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, Im a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button colorBool={this.state.showPersons} onClick={this.togglePersonsHandler}>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
+          <button className={btnClass.join(' ')} onClick={this.togglePersonsHandler}>
             Toggle Persons
             </button>
           {persons}
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import styled from 'styled-components'
 
-import './Person.css'
+import classes from './Person.css';
 
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
+const person = props => {
+  return (
+    <div className={classes.Person}>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
+};
 
-    @media (min-width: 500px){
-        width: 450px
-    }
-`;
-
-const person = (props) => {
-    return (
-        <StyledDiv>
-            <p onClick={props.click}>Im {props.name} and I am {props.age}!</p>
-            <pre>{props.children}</pre>
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </StyledDiv>
-    )
-}
-
-export default person
+export default person;
