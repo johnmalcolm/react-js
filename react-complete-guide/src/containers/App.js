12e2e450e59bed842f728b6c7651397c6e2d45f6
John Malcolm Anderson <jmanderson@verizonmedia.com>
2021-01-26 16:28:23 +0000

    Component lifecycle 2

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -5,6 +5,12 @@ import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit"
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    console.log('[App.js] constructor')
+
+  }
+
   state = {
     persons: [
       { id: '62dfs34', name: "Max", age: 29 },
@@ -13,6 +19,15 @@ class App extends Component {
     ],
     showPersons: false
   };
+
+  static getDerivedStateFromProps(props, state){
+    console.log('[App.js] getDerivedStateFromProps ', props)
+    return state
+  }
+
+  componentDidMount(){
+    console.log('[App.js] componentDidMount ')
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -43,6 +58,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] render');
     let persons = null;
 
     if (this.state.showPersons ){
