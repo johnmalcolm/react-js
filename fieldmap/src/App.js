9bfcf57ac9a314f5098e97c5277a8e11ebce6550
John Malcolm Anderson <jmanderson@verizonmedia.com>
2021-01-29 21:05:04 +0000

    fieldmap

---
+++ fieldmap/src/App.js
@@ -0,0 +1,15 @@
+import logo from './journal.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="full-logo">
+        <img id="logo" src={logo} />
+        <h1 id="brandmark">fieldmap</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
